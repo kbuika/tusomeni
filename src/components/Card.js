00c4a69b6c5ecb739d1502ab3397c2e668f3b6c2
kibuikaCodes <kibuikasteve@gmail.com>
2021-03-14 01:30:37 +0300

    :children_crossing: alter card tags

--- src/components/Card.js
+++ src/components/Card.js
@@ -50,41 +50,30 @@ const PaperYear = styled.p`
   color: ${colors.blue};
 `;
 
-const PaperTypeDiv = styled.div`
+const TopDiv = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  height: 1.5em;
-  width: auto;
   margin-right: 0.4em;
   margin-top: 0.2em;
-  padding-left: 0.5em;
-  padding-right: 0.5em;
-  border-radius: 10px;
-  background-color: ${colors.blue};
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
 
-const CourseTypeDiv = styled.div`
-  position: absolute;
-  top: 0;
-  right: 50px;
+const TypeDiv = styled.div`
   height: 1.5em;
   width: auto;
-  margin-right: 0.4em;
-  margin-top: 0.2em;
   padding-left: 0.5em;
   padding-right: 0.5em;
   border-radius: 10px;
+  margin-right: ${(props) => (props.space ? "0.3em" : "0")};
   background-color: ${colors.blue};
   display: flex;
   align-items: center;
-  justify-content: space-between;
 `;
 
-const PaperType = styled.p`
+const Type = styled.p`
   font-size: 0.7em;
   font-weight: 600;
   color: ${colors.light};
@@ -105,12 +94,14 @@ function Card({ paper }) {
             alt={paper.title}
           ></PaperImage>
         </ImageContainer>
-        <PaperTypeDiv>
-          <PaperType>{paper.paperCategory}</PaperType>
-        </PaperTypeDiv>
-        <CourseTypeDiv>
-          <PaperType>{paper.courseType}</PaperType>
-        </CourseTypeDiv>
+        <TopDiv>
+          <TypeDiv space>
+            <Type>{paper.yearOfStudy}</Type>
+          </TypeDiv>
+          <TypeDiv>
+            <Type>{paper.courseType}</Type>
+          </TypeDiv>
+        </TopDiv>
         <TextContainer>
           <PaperName>{paper.title}</PaperName>
           <PaperYear>{paper.year}</PaperYear>
