e18af92c4b5a63a9d96a801a07d871c91e7c4d53
kibuikaCodes <kibuikasteve@gmail.com>
2021-06-14 12:55:03 +0300

    :sparkles: add tawk.to and carousel

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,10 +5,82 @@ import Filter from "../components/FilterComponent/Filter";
 import Loaders from "../components/Loader/Loader";
 import { colors } from "../resources/ThemeColors";
 import { breakpoints } from "../Media";
+import CarouselComponent from "../components/Carousel/Carousel";
+
+class HomePage extends Component {
+  state = {
+    isError: false,
+    isLoading: false,
+    data: [],
+    searchData: [],
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("https://tusome-app.herokuapp.com/api/v1/papers/getAllPapers")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ data: data });
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => this.setState({ isError: true, isLoading: false }));
+  }
+
+  onHandleSearch = (selectedOption) => {
+    const { data } = this.state;
+    var newArray = data.filter(function (el) {
+      return el.yearOfStudy === selectedOption.value;
+    });
+    this.setState({ searchData: newArray });
+  };
+
+  render() {
+    return (
+      <MainDiv>
+        <NotifyDiv>
+          &#127882; <strong>Success in your exams.. You got this.</strong>
+        </NotifyDiv>
+        <CarouselDiv>
+          <CarouselComponent />
+        </CarouselDiv>
+        <FilterComponent>
+          <Filter onSearch={this.onHandleSearch} />
+        </FilterComponent>
+        {this.state.isError && (
+          <DisplayContainer>
+            <Error>Something went wrong... try agin later</Error>
+          </DisplayContainer>
+        )}
+        {this.state.isLoading && (
+          <DisplayContainer>
+            <Loaders />
+            <Error>Morio, punguza sup..</Error>
+          </DisplayContainer>
+        )}
+        {this.state.searchData.length !== 0 && (
+          <CardsContainer>
+            {this.state.searchData.map((paper) => (
+              <Card paper={paper} />
+            ))}
+          </CardsContainer>
+        )}
+        {this.state.searchData.length === 0 && (
+          <CardsContainer>
+            {this.state.data.map((paper) => (
+              <Card paper={paper} />
+            ))}
+          </CardsContainer>
+        )}
+      </MainDiv>
+    );
+  }
+}
+
+export default HomePage;
 
 const MainDiv = styled.div`
   height: auto;
-  width: auto;
+  width: 100%;
   margin: 0;
   z-index: -1;
   background-color: ${colors.darkish};
@@ -63,68 +135,8 @@ const NotifyDiv = styled.div`
   font-weight: 400;
 `;
 
-class HomePage extends Component {
-  state = {
-    isError: false,
-    isLoading: false,
-    data: [],
-    searchData: [],
-  };
-
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch("https://tusome-app.herokuapp.com/api/v1/papers/getAllPapers")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ data: data });
-        this.setState({ isLoading: false });
-      })
-      .catch((error) => this.setState({ isError: true, isLoading: false }));
-  }
-
-  onHandleSearch = (selectedOption) => {
-    const { data } = this.state;
-    var newArray = data.filter(function (el) {
-      return el.yearOfStudy === selectedOption.value;
-    });
-    this.setState({ searchData: newArray });
-  };
-
-  render() {
-    return (
-      <MainDiv>
-        <NotifyDiv>&#127882; Success in your exams..</NotifyDiv>
-        <FilterComponent>
-          <Filter onSearch={this.onHandleSearch} />
-        </FilterComponent>
-        {this.state.isError && (
-          <DisplayContainer>
-            <Error>Something went wrong... try agin later</Error>
-          </DisplayContainer>
-        )}
-        {this.state.isLoading && (
-          <DisplayContainer>
-            <Loaders />
-            <Error>Morio, punguza sup..</Error>
-          </DisplayContainer>
-        )}
-        {this.state.searchData.length !== 0 && (
-          <CardsContainer>
-            {this.state.searchData.map((paper) => (
-              <Card paper={paper} />
-            ))}
-          </CardsContainer>
-        )}
-        {this.state.searchData.length === 0 && (
-          <CardsContainer>
-            {this.state.data.map((paper) => (
-              <Card paper={paper} />
-            ))}
-          </CardsContainer>
-        )}
-      </MainDiv>
-    );
-  }
-}
-
-export default HomePage;
+const CarouselDiv = styled.div`
+  width: 90vw;
+  height: 20vh;
+  margin: 3em;
+`;
