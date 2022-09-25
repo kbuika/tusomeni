93dfa0479493a42041dd9d7692749e3a16fcecc9
Steve Kibuika <kibuikasteve@gmail.com>
2022-09-25 10:32:52 +0300

    restore sumbit papers page

--- src/pages/CollectPapers.js
+++ src/pages/CollectPapers.js
@@ -7,13 +7,14 @@ class CollectPapers extends Component {
       <MainDiv>
         {/* <h2>Coming Soon</h2> */}
         <p>
-          We are having a hard time getting papers from the administartion, so
-          what better way than to collect from students?
+          If you're in 1st year or 2nd year. Keep all the papers for the upcoming exams and immediately you have all of them, email me 
+          so I can buy the papers from you.
         </p>
         <p style={{width: '80%'}}>
-          If you have a proper past-papers collection, send an email to
-           <a href="mailto:thekibuika@gmail.com"> thekibuika@gmail.com</a> indicating the papers you have and their
-          respective years and you will receive compensations of up to <strong>KES 500</strong> or more.
+          The papers will be bought on a bidding process. Whoever has the entire collection and the best bid wins!
+        </p>
+        <p style={{width: '80%'}}>
+          Looking forward to your emails.
         </p>
         <p>You in?</p>
         <a href="mailTo:thekibuika@gmail.com">Email Kibuika</a>
